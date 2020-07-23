050349b76760081d0d1c3d3eb34fcc526e53f77d
Jolanta Jasiulionyte <klpdjolanta@gmail.com>
2020-07-23 13:40:20 +0100

    removes unused files

--- services/treatData.js
+++ services/treatData.js
@@ -1,7 +1,7 @@
 const csv = require('csv-parser')
 const fs = require('fs')
 const translateRow = require('./googleTranslateClient')
-const ewc_descriptions = require('./private/originals/EWCcodeDescriptions')
+const ewc_descriptions = require('../private/originals/EWCcodeDescriptions')
 let results = []
 
 const createEuralDescritpionsCSV = async () => {
@@ -89,7 +89,7 @@ const getEuralCodeDescriptions = () => {
 
 const getUniqueMaterials = () => {
   let uniqueMaterials = []
-  fs.createReadStream('./private/sanitized/enData.csv')
+  fs.createReadStream('./private/originals/nlData.csv')
     .pipe(csv())
     .on('data', (row) => {
       const {
@@ -100,14 +100,101 @@ const getUniqueMaterials = () => {
         directProduct,
         indirectProduct
       } = row
-      let material = indirectProduct && indirectProduct.toLowerCase()
+      let material = material4 && material4.toLowerCase()
       if (!uniqueMaterials.filter((each) => each === material).length)
         uniqueMaterials.push(material)
     })
     .on('end', () => {
-      saveFile('enUniqueMaterials', JSON.stringify(uniqueMaterials))
+      saveFile('nlUniqueMaterials', JSON.stringify(uniqueMaterials))
       console.log(uniqueMaterials)
     })
 }
 
-// getUniqueMaterials()
+getUniqueMaterials()
+
+fillUnclassifiedFields = () => {
+  let richData = []
+  fs.createReadStream('./private/originals/nlData.csv')
+    .pipe(csv())
+    .on('data', (row) => {
+      const NA = 'unclassified'
+      let {
+        reason,
+        origin,
+        color,
+        state,
+        size,
+        consistency,
+        otherCode,
+        material4,
+        material3,
+        material2,
+        material,
+        mType,
+        composite2,
+        composite1,
+        cType,
+        indirectProduct,
+        directProduct,
+        pType,
+        mixedOrPure,
+        cleanOrDirty,
+        euralDescription,
+        euralCode,
+        description
+      } = row
+      if (reason === '') reason = NA
+      if (origin === '') origin = NA
+      if (color === '') color = NA
+      if (state === '') state = NA
+      if (size === '') size = NA
+      if (consistency === '') consistency = NA
+      if (otherCode === '') otherCode = NA
+      if (material4 === '') material4 = NA
+      if (material3 === '') material3 = NA
+      if (material2 === '') material3 = NA
+      if (material === '') material = NA
+      if (mType === '') mType = NA
+      if (composite2 === '') composite2 = NA
+      if (composite1 === '') composite1 = NA
+      if (cType === '') cType = NA
+      if (indirectProduct === '') indirectProduct = NA
+      if (directProduct === '') directProduct = NA
+      if (pType === '') pType = NA
+      if (mixedOrPure === '') mixedOrPure = NA
+      if (cleanOrDirty === '') cleanOrDirty = NA
+      if (euralDescription === '') euralDescription = NA
+      if (euralCode === '') euralCode = NA
+      if (description === '') description = NA
+      const richRow = {
+        reason,
+        origin,
+        color,
+        state,
+        size,
+        consistency,
+        otherCode,
+        material4,
+        material3,
+        material2,
+        material,
+        mType,
+        composite2,
+        composite1,
+        cType,
+        indirectProduct,
+        directProduct,
+        pType,
+        mixedOrPure,
+        cleanOrDirty,
+        euralDescription,
+        euralCode,
+        description
+      }
+      richData.push(richRow)
+    })
+    .on('end', () => {
+      saveFile('nlDataRich', JSON.stringify(richData))
+      console.log('all file saved-----')
+    })
+}
